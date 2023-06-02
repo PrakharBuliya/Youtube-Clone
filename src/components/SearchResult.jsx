f2f7b7c2409e2068851b33cceec83be1c30165af
Prakhar Buliya <prakharbuliya02@gmail.com>
2023-06-03 02:37:33 +0530

    Added search functionality

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -9,7 +9,6 @@ import SearchResultVideoCard from "./SearchResultVideoCard";
 const SearchResult = () => {
   const [result, setResult] = useState();
   const { searchQuery } = useParams();
-
   const { setLoading } = useContext(Context);
 
   useEffect(() => {
@@ -26,7 +25,20 @@ const SearchResult = () => {
     });
   };
 
-  return <div>SearchResult</div>;
+  return (
+    <div className="flex flex-row h-[calc(100%-56px)]">
+      <LeftNav />
+      <div className="grow w-[calc(100%-240px)] h-full overflow-y-auto bg-black">
+        <div className="grid grid-cols-1 gap-2 p-5">
+          {result?.map((item) => {
+            if (item?.type !== "video") return false;
+            let video = item.video;
+            return <SearchResultVideoCard key={video.videoId} video={video} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SearchResult;
